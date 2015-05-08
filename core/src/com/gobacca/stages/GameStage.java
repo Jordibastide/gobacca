a8773ea21a08b3f439b718c484abb268af3f3bc0
kingofthebongo <carpentier.thibaud@gmail.com>
2015-05-08 16:34:04 +0200

    Program-structure-step-4

--- core/src/com/gobacca/stages/GameStage.java
+++ core/src/com/gobacca/stages/GameStage.java
@@ -6,15 +6,19 @@ import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.physics.box2d.*;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.Scaling;
+import com.badlogic.gdx.utils.viewport.ScalingViewport;
 import com.gobacca.actors.*;
 import com.gobacca.utils.*;
 
 
-public class GameStage extends Stage implements ContactListener {
+public class GameStage extends Stage implements ContactListener
+{
 
     // This will be our viewport measurements while working with the debug renderer
-    private static final int VIEWPORT_WIDTH = 20;
-    private static final int VIEWPORT_HEIGHT = 13;
+	private static final int VIEWPORT_WIDTH = Constants.APP_WIDTH;
+    private static final int VIEWPORT_HEIGHT = Constants.APP_HEIGHT;
 
     private World world;
     private Ground ground;
@@ -32,6 +36,7 @@ public class GameStage extends Stage implements ContactListener {
 
     public GameStage()
     {
+    	super(new ScalingViewport(Scaling.stretch, VIEWPORT_WIDTH, VIEWPORT_HEIGHT, new OrthographicCamera(VIEWPORT_WIDTH, VIEWPORT_HEIGHT)));
         initWorld();
         initCamera();
         initTouchControlAreas();
@@ -40,10 +45,17 @@ public class GameStage extends Stage implements ContactListener {
 
     private void initWorld()
     {
-        world = WorldUtils.createWorld();
+    	world = WorldUtils.createWorld();
         world.setContactListener(this);
+        initBackground();
         initGround();
         initNinja();
+        createEnemy();
+    }
+    
+    private void initBackground()
+    {
+        addActor(new Background());
     }
 
     private void initGround()
@@ -78,7 +90,14 @@ public class GameStage extends Stage implements ContactListener {
     {
         super.act(delta);
 
-        // Fixed timestep
+        Array<Body> bodies = new Array<Body>(world.getBodyCount());
+        world.getBodies(bodies);
+
+        for (Body body : bodies) {
+            update(body);
+        }
+
+        // Fix timestep
         accumulator += delta;
 
         while (accumulator >= delta) {
@@ -87,12 +106,14 @@ public class GameStage extends Stage implements ContactListener {
         }
     }
 
+    /*
     @Override
     public void draw()
     {
         super.draw();
         renderer.render(world, camera.combined);
     }
+    */
     
     @Override
     public boolean touchDown(int x, int y, int pointer, int button)
@@ -118,34 +139,58 @@ public class GameStage extends Stage implements ContactListener {
         getCamera().unproject(touchPoint.set(x, y, 0));
     }
     
-    //@Override
+    private void update(Body body)
+    {
+        if (!BodyUtils.bodyInBounds(body))
+        {
+            if (BodyUtils.bodyIsEnemy(body) && !ninja.isHit())
+            {
+                createEnemy();
+            }
+            world.destroyBody(body);
+        }
+    }
+
+    private void createEnemy()
+    {
+        Enemy enemy = new Enemy(WorldUtils.createEnemy(world));
+        addActor(enemy);
+    }
+    
+    @Override
     public void beginContact(Contact contact)
     {
         Body a = contact.getFixtureA().getBody();
         Body b = contact.getFixtureB().getBody();
 
-        if ((BodyUtils.bodyIsNinja(a) && BodyUtils.bodyIsGround(b)) || (BodyUtils.bodyIsGround(a) && BodyUtils.bodyIsNinja(b)))
+        if((BodyUtils.bodyIsNinja(a) && BodyUtils.bodyIsEnemy(b)) || (BodyUtils.bodyIsEnemy(a) && BodyUtils.bodyIsNinja(b)))
         {
-            ninja.landed();
+            ninja.hit();
         }
+        else if((BodyUtils.bodyIsNinja(a) && BodyUtils.bodyIsGround(b)) || (BodyUtils.bodyIsGround(a) && BodyUtils.bodyIsNinja(b)))
+        {
+        	ninja.landed();
+        }
+
     }
     
-    //@Override
+    @Override
     public void endContact(Contact contact)
     {
 
     }
 
-    //@Override
+    @Override
     public void preSolve(Contact contact, Manifold oldManifold)
     {
 
     }
 
-    //@Override
+    @Override
     public void postSolve(Contact contact, ContactImpulse impulse)
     {
 
     }
+
 
 }
